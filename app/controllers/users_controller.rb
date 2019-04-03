4f868c8ffc3e940d790fd2a7c9125c1629d43dbd
mymedog1005 <mymedog1005@gmail.com>
2019-04-03 15:51:55 +0000

    basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-       flash[:success] = "ユーザーの新規作成に成功しました。"
+      log_in @user
+      flash[:success] = "ユーザーの新規作成に成功しました。"
       redirect_to @user
     else
       render 'new'
